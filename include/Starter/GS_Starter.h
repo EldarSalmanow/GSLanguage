fa551a13ca3124d8dd58ab905b9f97dde8907c5c
Eldar <eldar.salm@gmail.com>
2021-02-01 15:30:23 +0300

    Adding GS_Starter class for start compiler

---
+++ include/Starter/GS_Starter.h
@@ -0,0 +1,44 @@
+#ifndef GSLANGUAGE_GS_STARTER_H
+#define GSLANGUAGE_GS_STARTER_H
+
+#include "../../include/Compiler/Util/GS_Arguments.h"
+#include "../../include/Compiler/Reader/GS_Reader.h"
+#include "../../include/Compiler/Lexer/GS_Lexer.h"
+#include "../../include/Compiler/Parser/GS_Parser.h"
+
+namespace Starter {
+
+    using namespace GSLanguageCompiler;
+
+    /**
+     * Class for starting GSLanguageCompiler
+     */
+    class GS_Starter {
+    public:
+        /**
+         * Starting compiler
+         * @param argc Number of arguments
+         * @param argv Array of arguments
+         * @return Status number for operation system
+         */
+        static int startCompiler(int argc, char *argv[]);
+
+    private:
+        /**
+        * Start compiling function
+        * @param arguments Command line arguments before argument analyzing
+        */
+        static void startCompiling(GSArgumentsPointer &arguments);
+
+        /**
+        * Function for parsing command line arguments and generating config for compiling
+        * @param argc Number of arguments
+        * @param argv Array of arguments
+        * @return Configs for compiling
+        */
+        static GSArgumentsPointer parseArguments(int argc, char **argv);
+    };
+
+}
+
+#endif //GSLANGUAGE_GS_STARTER_H
---
+++ src/Starter/GS_Starter.cpp
@@ -0,0 +1,57 @@
+#include "../../include/Starter/GS_Starter.h"
+#include "../../test/Tests.h"
+
+namespace Starter {
+
+    int GS_Starter::startCompiler(int argc, char **argv) {
+        try {
+            GSArgumentsPointer arguments = parseArguments(argc, argv);
+
+            if (arguments == nullptr) {
+                return 1;
+            }
+
+            startCompiling(arguments);
+
+        } catch (Exceptions::_GS_Exception &exception) {
+            std::cerr << exception._getErrorMessage() << std::endl;
+
+            return 1;
+        } catch (std::exception &exception) {
+            std::cerr << exception.what() << std::endl;
+
+            return 1;
+        }
+        return 0;
+    }
+
+    void GS_Starter::startCompiling(GSArgumentsPointer &arguments) {
+
+        // reading input file
+        GSReaderPointer reader(new GS_Reader(arguments->getFilename()));
+        GSText input = reader->readFile();
+
+        // lexer analyzing
+        GSLexerPointer lexer(new GS_Lexer(input));
+        GSTokenArray tokens = lexer->tokenize();
+
+        // lexer testing
+        if (arguments->getIsTestingMode()) {
+            Testing::printTokenTypes(tokens);
+        }
+    }
+
+    GSArgumentsPointer GS_Starter::parseArguments(int argc, char *argv[]) {
+        GSArgumentsPointer arguments(new GS_Arguments(argc, argv));
+
+        if (argc < 3) {
+            arguments->printUsage();
+            return nullptr;
+        }
+
+        arguments->parseArguments();
+
+        return arguments;
+    }
+
+}
