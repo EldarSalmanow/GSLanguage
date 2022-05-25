f4c759103b24f82d94118e083e096682dcb5a65e
EldarSalmanow <eldar.salm@gmail.com>
2022-05-25 12:03:38 +0300

    Adding not working examples (for future) and updates

---
+++ examples/api/TUCreation/main.cpp
@@ -0,0 +1,39 @@
+#include <AST/AST.h>
+
+/**
+ * Simple example of creating translation unit with AST builder
+ * @return Translation unit declaration ptr
+ */
+AST::GSTranslationUnitDeclarationPtr CreateUnit() {
+    /**
+     *
+     * main.gs
+     *
+     * func main() {
+     *
+     * }
+     *
+     */
+
+    // create AST builder
+    auto Builder = AST::GS_ASTBuilder::Create();
+
+    // create 'main' translation unit declaration
+    auto Unit = Builder->CreateTranslationUnitDeclaration("main");
+
+    // create 'main' function signature
+    auto FunctionSignature = Builder->CreateFunctionSignature(Builder->CreateI32Type());
+
+    // create 'main' function
+    auto Function = Builder->CreateFunctionDeclaration("main", FunctionSignature);
+
+    // adding 'main' function to translation unit declaration
+    Unit->AddNode(Function);
+
+    // return translation unit declaration
+    return Unit;
+}
+
+I32 main(I32 argc, Ptr<Ptr<C8>> argv) {
+    return 0;
+}
