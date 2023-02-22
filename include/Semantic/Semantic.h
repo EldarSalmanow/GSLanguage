70c206205543a7a87296ad881c3e03d3d4d03a60
Eldar <eldar.salm@gmail.com>
2023-02-22 22:39:54 +0300

    Passing Semantic audit

--- include/Semantic/Semantic.h
+++ include/Semantic/Semantic.h
@@ -5,6 +5,19 @@
  * This file includes all files from GSLanguageCompiler Semantic Module
  */
 
+/*
+ *
+ * AUDIT -
+ *
+ * CMakeLists.txt - Pass
+ * Semantic.h - Pass
+ * GS_Type.h - Pass
+ * GS_TableOfSymbols.h - Pass
+ * Passes/SemanticPasse.h - Pass
+ * Passes/GS_SymbolsPlaceholderPass.h - Pass
+ *
+ */
+
 #include <Semantic/GS_Type.h>
 
 #include <Semantic/GS_TableOfSymbols.h>
