8447da24fd0982a970dcc7f527fe1fe418f5c132
Eldar <eldar.salm@gmail.com>
2023-02-22 22:36:08 +0300

    Passing Lexer audit

--- include/Lexer/Lexer.h
+++ include/Lexer/Lexer.h
@@ -5,6 +5,18 @@
  * This file includes all files from GSLanguageCompiler Lexer Module
  */
 
+/*
+ *
+ * AUDIT - Pass
+ *
+ * CMakeLists.txt - Pass
+ * Lexer.h - Pass
+ * GS_TokenTypes.def - Pass
+ * GS_Token.h - Pass
+ * GS_Lexer.h - Pass
+ *
+ */
+
 #include <Lexer/GS_Token.h>
 
 #include <Lexer/GS_Lexer.h>
